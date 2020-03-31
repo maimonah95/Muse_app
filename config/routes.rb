e36bd166c3e90db2e77740ff3130f4be11b94268
maimonah95 <malhwaymil@gmail.com>
2020-03-31 10:33:17 +0300

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'artists#index'
-  resources :artists
-  get "artists", to: "artists#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists ,:songs
+  # get "artists", to: "artists#index"
+  # get "artists/:id", to: "artists#show", as: :artist
 end
